b92dc392e5893028fa3629c35ab9b9d7429a2fa0
rebornix <penn.lv@gmail.com>
2023-03-20 16:46:02 -0700

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "nb-will-save" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.workspace.onWillSaveNotebookDocument(event => {
+		if (event.reason == vscode.NotebookDocumentSaveReason.Manual) {
+			event.waitUntil(new Promise((resolve) => {
+				const notebookEdit = new vscode.NotebookEdit(new vscode.NotebookRange(0, 0), [new vscode.NotebookCellData(vscode.NotebookCellKind.Code, 'print(1)', 'python')]);
+				const edit = new vscode.WorkspaceEdit();
+				edit.set(event.document.uri, [notebookEdit]);
+				resolve([edit]);
+			}));
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
